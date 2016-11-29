74e0ba45bab2f1f1eeb670762a3f802365b4035f
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:54:18 +1300

    PATCH: module clean-up

--- code/forms/gridfield/GridFieldAddNewButtonOriginalPage.php
+++ code/forms/gridfield/GridFieldAddNewButtonOriginalPage.php
@@ -13,45 +13,45 @@
  * @package forms
  * @subpackage fields-gridfield
  */
-class GridFieldAddNewButtonOriginalPage extends GridFieldAddNewButton {
+class GridFieldAddNewButtonOriginalPage extends GridFieldAddNewButton
+{
+    public function getHTMLFragments($gridField)
+    {
+        $singleton = singleton($gridField->getModelClass());
+
+        if (!$singleton->canCreate()) {
+            return array();
+        }
+
+        if (!$this->buttonName) {
+            // provide a default button name, can be changed by calling {@link setButtonName()} on this component
+            $objectName = $singleton->i18n_singular_name();
+            $this->buttonName = _t('GridField.Add_USING_PAGES_SECTION', 'Add {name} using pages section', array('name' => $objectName));
+        }
+
+        $getSegment = "";
+        if ($page = $this->BestParentPage()) {
+            $getSegment = "?ParentID=".$page->ID;
+        }
+
+        $data = new ArrayData(array(
+            'NewLink' => "/admin/".Config::inst()->get("CMSPageAddController_Products", "url_segment")."/".$getSegment,
+            'ButtonName' => $this->buttonName,
+        ));
+
+        return array(
+            $this->targetFragment => $data->renderWith('GridFieldAddNewbutton'),
+        );
+    }
 
 
-	public function getHTMLFragments($gridField) {
-		$singleton = singleton($gridField->getModelClass());
-
-		if(!$singleton->canCreate()) {
-			return array();
-		}
-
-		if(!$this->buttonName) {
-			// provide a default button name, can be changed by calling {@link setButtonName()} on this component
-			$objectName = $singleton->i18n_singular_name();
-			$this->buttonName = _t('GridField.Add_USING_PAGES_SECTION', 'Add {name} using pages section', array('name' => $objectName));
-		}
-
-		$getSegment = "";
-		if($page = $this->BestParentPage()) {
-			$getSegment = "?ParentID=".$page->ID;
-		}
-
-		$data = new ArrayData(array(
-			'NewLink' => "/admin/".Config::inst()->get("CMSPageAddController_Products", "url_segment")."/".$getSegment,
-			'ButtonName' => $this->buttonName,
-		));
-
-		return array(
-			$this->targetFragment => $data->renderWith('GridFieldAddNewbutton'),
-		);
-	}
-
-
-	/**
-	 * finds the most likely root parent for the shop
-	 *
-	 * @return SiteTree | NULL
-	 */
-	public function BestParentPage(){
-		return SiteTree::get()->filter("ParentID", 0)->First();
-	}
-
+    /**
+     * finds the most likely root parent for the shop
+     *
+     * @return SiteTree | NULL
+     */
+    public function BestParentPage()
+    {
+        return SiteTree::get()->filter("ParentID", 0)->First();
+    }
 }
